ad2d6f2e2ab02ba0be90a835044bdbc75e75a73e
Soumajit Das <Cykeek@proton.me>
2023-09-26 21:07:03 +0530

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+# clear out screen
+clear
+
 banner(){
 echo "###########################"
 echo "#                         #"
@@ -7,6 +10,7 @@ echo "# Kernel Build Script     #"
 echo "# For msm4.9 Kernel Based #"
 echo "#                         #"
 echo "###########################"
+echo
 }
 
 # Define colors
@@ -30,25 +34,35 @@ red_message() {
   echo -e "$red$1$white"
 }
 
-# Cleanup old compilation artifacts
-green_message "<< Cleanup >>"
-rm -rf out/
-rm -rf anykernel/
-yellow_message "<< Cleaned >>"
+clean() {
+  echo
+  # Clean out residuals
+  red_message "<< Clean out residuals!! >>"
+  sleep 1s
+  echo
+  rm -rf out/
+  rm -rf anykernel/
+  green_message "<< Residuals Cleaned!! >>"
+}
 
 # Input branch name and folder name
-read -p "Enter Your Branch name: " branch
-read -p "Folder Name You Want to Save: " folder
+branch='stable'
+folder='sdm710'
+repo_link='https://github.com/Cykeek-Labs/kernel_realme_sdm710'
+green_message "File will be saved in $folder"
+sleep 1s
 
 # Check if the folder already exists
 if [ -d "$folder" ]; then
+        echo
         yellow_message "This $folder is already saved, nothing to clone!"
         cd $folder
+        clean
         echo
 else
-        yellow_message "Downloading your files..."
+        yellow_message "Downloading your files from $repo_link from branch $branch....."
         echo
-        git clone https://github.com/Cykeek-Labs/kernel_realme_sdm710 -b "$branch" "$folder"
+        git clone $repo_link -b "$branch" $folder
         cd $folder
         yellow_message "Your files have been successfully saved in $folder"
         echo
@@ -60,7 +74,8 @@ DEVICE="Realme 3 Pro"
 CODENAME="RMX1851"
 KERNEL_NAME="Meraki-SDM710"
 KERNEL_VER="4.9"
-yellow_message "OEM & Kernel Information Accepted"
+echo
+yellow_message "Device Name is $DEVICE and Kernel Name is $KERNEL_NAME"
 sleep 1s
 echo
 
@@ -80,7 +95,7 @@ echo
 # Define KBUILD Information
 HOST="HyperServers"
 USER="Cykeek"
-yellow_message "Host Build Set to\nHOST=$HOST and USER=$USER"
+yellow_message "Host Build Set to \nHOST=$HOST and USER=$USER"
 sleep 1s
 echo
 
@@ -95,6 +110,7 @@ TOOLCHAIN_SOURCE="https://gitlab.com/PixelOS-Devices/playgroundtc.git"
 GCC_Source_32="https://github.com/mvaisakh/gcc-arm"
 GCC_Source_64="https://github.com/mvaisakh/gcc-arm64"
 
+# Automation for toolchain and gcc builds
 if [ "$TOOLCHAIN" == "gcc" ]; then
     if [ ! -d "$HOME/cykeek/gcc64" ] && [ ! -d "$HOME/cykeek/gcc32" ]; then
       yellow_message "Your Choose $TOOLCHAIN"
@@ -123,7 +139,10 @@ fi
 # Function to build the kernel
 build_kernel() {
         green_message "Executing kernel Build..."
+        sleep 1s
+        echo
         echo "Your Kernel Version is $KERNEL_VER"
+        echo
         sleep 1s
         Start=$(date +"%s")
 
@@ -171,14 +190,19 @@ export KBUILD_BUILD_USER="$USER"
 
 # Pre-configured Actions
 green_message "Creating workspace for Kernel. Please Wait!"
+echo
 sleep 1s
 mkdir -p out/
-yellow_message "<< Copying dts >>"
+yellow_message "<< Copying DTS Files!! >>"
+echo
+sleep 1s
 mkdir -p out/arch/arm64/
 cp -r arch/arm64/boot out/arch/arm64/
 rm -rf out/arch/arm64/boot/dts/qcom
 mkdir -p out/arch/arm64/boot/dts/qcom/
 cp arch/arm64/boot/dts/qcom/*.dts* out/arch/arm64/boot/dts/qcom/
+green_message "<< DTS copied successfully!! >>"
+echo
 sleep 1s
 
 make O=out clean && make O=out mrproper
@@ -193,50 +217,104 @@ DATE=$(date +"%Y%m%d-%H%M%S")
 KERVER=$(make kernelversion)
 if [ -f "$IMG" ]; then
         green_message "<< Build completed in $(($Diff / 60)) minutes and $(($Diff % 60)) seconds >>"
+        sleep 2s
         echo
 
         # Now Clone AnyKernel
+        clear
+        sleep 2s
         yellow_message "<< Cloning AnyKernel from your repo >>"
         git clone "$AnyKernel" --single-branch -b "$AnyKernelbranch" anykernel
         echo
+        green_message "<< AnyKernel Cloned Successfully!! >>"
 
         # Move Images into anykernel Folder
         yellow_message "<< Making kernel zip >>"
         sleep 1s
-        cp -r "$IMG" anykernel/
+        cp -r "$IMG" anykernel/ # Copy $IMG into anykernel folder
+        rm -rf *zip # Remove previous residuals if have any!!
         cd anykernel
         mv Image.gz-dtb zImage
         export ZIP="$KERNEL_NAME"-"$CODENAME"-"$DATE"
         echo
 
         # Zip it
+        yellow_message "<< Zipping Imgs...>>"
+        sleep 2s
         zip -r "$ZIP" *
         curl -sLo zipsigner-3.0.jar https://raw.githubusercontent.com/Hunter-commits/AnyKernel/master/zipsigner-3.0.jar
         java -jar zipsigner-3.0.jar "$ZIP".zip "$ZIP"-signed.zip
-        green_message "Files processed. The file name is $ZIP.zip. Please Wait...."
+        green_message "Files processed. The file name is $ZIP.zip Please Wait...."
         echo
         sleep 1s
 
         # Check Upload server Connection is alive or not
+        # banner
+        upload_banner() {
+          clear
+          sleep 2s
+          echo "###########################################"
+          sleep 1s
+          echo "#             UPLOADING                   #"
+          sleep 1s
+          echo "# NOTICE: Don't Upload any Pirate Stuff   #"
+          sleep 1s
+          echo "# in devuploads or temp.sh u might kill   #"
+          sleep 1s
+          echo "# their services with copyrights like our #"
+          sleep 1s
+          echo "# beloved temp.sh #RIP                    #"
+          sleep 1s
+          echo "###########################################"
+          sleep 1s
+          echo
+        }
+        # Temp.sh
         server_address="temp.sh"
+
+        # Devuploads
+        Server_Name="devuploads.com" # before adding any website name check whethere it's pinging or not like 'ping google.com'
+        server_address1="https://devuploads.com/upload.sh" # Specific to Upload server upload automtion!! refer to website or contact admin
+        KEY="" # Devuploads provide API key to establish connections
+
+        # Custom
         custom_service="/var/www/html/RMX1851/"
 
         # Check if the server is alive using ping
         if ping -c 1 "$server_address" &> /dev/null; then
+                upload_banner
                 green_message "Server $server_address is alive."
                 red_message "Uploading..."
                 echo
                 sleep 1s
                 curl -T "$ZIP".zip temp.sh
                 red_message "Uploaded!!"
+                cd ..
+                clean
+                banner
+                exit 1
+        elif ping -c 1 "$Server_Name" &> /dev/null; then
+                upload_banner
+                green_message "Devuploads is Alive."
+                red_message "Uploading..."
+                echo
+                sleep 1s
+                bash <(curl -s $server_address1) -f "$ZIP".zip -k $KEY
+                sleep 1s
+                echo "$ZIP.zip is uploaded!!"
+                cd ..
+                clean
+                banner
+                exit 1
         else
                 # custom command for custom upload folder
+                upload_banner
                 green_message "$server_address is down."
                 green_message "as of now upload service has been switched to $custom_service"
                 sleep 2s
                 echo
                 echo "Cleanup Previous Uploaded Folder!!"
-                rm -rf /var/www/html/RMX1851/Meraki-SDM710-RMX1851-*.zip
+                rm -rf /var/www/html/RMX1851/Meraki-SDM710-RMX1851-*.zip # clean out previous similar files to avoid conflicts @_@
                 echo
                 sleep 1s
                 green_message "residuals Cleaned up in $custom_service"
@@ -248,15 +326,16 @@ if [ -f "$IMG" ]; then
                 red_message "Uploaded!!"
                 echo
                 sleep 1s
-                echo "Please head to http://94.130.205.48/RMX1851/"
-                echo "to download $ZIP.zip"
+                echo "Please head to http://94.130.205.48/RMX1851/$ZIP.zip"
                 sleep 1s
                 cd ..
+                clean
                 banner
-                exit
+                exit 1
         fi
 else
         red_message "Failed to compile the kernel, check output for errors."
         cat out/error.log | curl -F 'f:1=<-' ix.io
+        clean
         exit 1
 fi
